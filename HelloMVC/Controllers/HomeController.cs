6a26da46712ad5043541433df2186953eb85e12c
Makoto Mizukami <makoto@circleci.com>
2020-05-15 18:54:11 +0900

    Init

---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "This is our about page";
+            ViewBag.Message = "do diese";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact us";
+            ViewBag.Message = "Call me maybe.";
+
+            return View();
+        }
+    }
+}
